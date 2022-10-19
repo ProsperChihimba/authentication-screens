d0222f5c822b74a6c25498a5803643827e63fc45
ProsperChihimba <prosperchihimba@gmail.com>
2022-10-20 00:19:16 +0300

    Add: inter font & right side logo

--- src/components/rightSide/index.jsx
+++ src/components/rightSide/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
+import { Box, Image, Flex, Text } from '@chakra-ui/react';
+import logo from '../../assets/logo.png'
 
 const RightSide = () => {
     return (
-        <Box w='50%' bg='#040308' h='100vh'>RightSide</Box>
+        <Box w='50%' bg='#040308' h='100vh' p='100px' fontFamily='Inter'>
+            <Flex>
+                <Flex gap='30px'>
+                    <Image src={logo} w='55px' h='40px' />
+                    <Text
+                        color='#FFFFFF'
+                        fontWeight='700'
+                        fontSize='30px'
+                    >
+                        DopeSaas
+                    </Text>
+                </Flex>
+            </Flex>
+        </Box>
     )
 }
 
