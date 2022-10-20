e90fb617597d2cd33a9f3050309d78c883b5af4e
ProsperChihimba <prosperchihimba@gmail.com>
2022-10-21 00:47:47 +0300

    Add: verify email component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import Main from './components/main';
+import Verify from './components/Verify';
 
 function App() {
   return (
     <ChakraProvider>
-      <Main />
+      <Verify />
     </ChakraProvider>
   );
 }
---
+++ src/components/Verify/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Flex,   } from '@chakra-ui/react'
+import RightSide from '../rightSide'
+
+const Verify = () => {
+    return (
+        <Flex>
+            <RightSide />
+            <div>Verify</div>
+        </Flex>
+    )
+}
+
+export default Verify
