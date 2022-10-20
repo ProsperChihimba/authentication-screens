a514f5637d38ba9d662775058632d377f5db128d
ProsperChihimba <prosperchihimba@gmail.com>
2022-10-21 00:57:07 +0300

    Update: finish up verify email design

--- src/components/Verify/index.jsx
+++ src/components/Verify/index.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
-import { Flex,   } from '@chakra-ui/react'
+import { Flex, Box, Text, Button } from '@chakra-ui/react'
 import RightSide from '../rightSide'
 
 const Verify = () => {
     return (
         <Flex>
             <RightSide />
-            <div>Verify</div>
+            <Box pr='100px' background='white' pl='100px' pt='200px' fontFamily='Inter'>
+                <Text
+                    as='b'
+                    color='#040308'
+                    fontWeight='900'
+                    fontSize='28px'
+                    mb='20px'
+                >
+                    Verify your Email
+                </Text>
+                <Text
+                    color='#040308'
+                    fontSize='12px'
+                    mb='70px'
+                >
+                    We have sent a verification email to n****e@e***e.com.
+                </Text>
+                <Text
+                    color='#040308'
+                    fontSize='12px'
+                    mb='20px'
+                >
+                    Didn’t receive the email? Check spam or promotion folder or
+                </Text>
+                <Button mb='20px' bg='#312ECB' color='white' size='sm' width='100%' fontSize='12px'>Resend Email</Button>
+            </Box>
         </Flex>
     )
 }
